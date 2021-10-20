ee1facc78de88c30cbb0e5c715f6e11959f793c3
Thomas Kerby <63968823+TXOG@users.noreply.github.com>
2021-10-20 19:52:17 +0100

    Create while repeat loop.cpp

---
+++ while repeat loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// Include the string library
+#include <string>
+
+
+
+int main(int, char**) {
+    while (true){
+        cout << "Enter any integer: ";
+    int x;
+    cin >> x;
+    cout << "You entered the number: " << x << "\n" << "Enter a string: ";
+    string im_using_a_string;
+    cin >> im_using_a_string; 
+    cout << "You entered: " << im_using_a_string;
+    cout << "\n";
+    }
+}
